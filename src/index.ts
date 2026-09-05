a8ac2f4493f0e9e74d2dd3586ad9bcb68bc5ad7a
Essohcod <nessoh1007@gmail.com>
2026-09-05 09:27:11 +0100

    voicenote

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ const app: Application = express();
 
 // Middleware configuration
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Root welcome endpoint
 app.get("/", (_req: Request, res: Response) => {
